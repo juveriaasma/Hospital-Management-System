6bb64db51dca74bf96eb6896654d6661654cb164
juveriaasma <juveriaasma5@gmail.com>
2023-08-04 00:00:13 +0530

    feat: start django project

---
+++ HospitalManagementSystem_django/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for HospitalManagementSystem_django project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HospitalManagementSystem_django.settings')
+
+application = get_wsgi_application()
